1f19c6359c22f71d86fa5fa2fdcc7751e4fc410b
Code Whisperer <daniel@danielstern.ca>
2016-06-01 12:58:37 -0400

    added support for queries, reqs

--- main.js
+++ main.js
@@ -15,16 +15,26 @@ app.use(express.static(`./client`));
 app.use('/api',express.static(`./docs`));
 app.route('/api/cities')
 	.get((req,res)=>{
+		let count = 50 || req.query.count;
+		let start = 0 || req.query.start;
+		
 		res.status(200)
-			.json(cities);
+			.json(cities.slice(start,count));
 	})
 
 app.route('/api/cities/:id')
 	.get((req,res)=>{
-		console.log(req.params);
-		//res.send('ok');
-		let city = cities.find(c=>c.id=req.params.id);
-		res.json(city);
+		// Find the city in the database with the matching ID
+		let city = cities.find(c=>c.id==req.params.id);
+		if (city) {
+			res.status(300)
+				.json(city);	
+		} else {
+			// Let the client know its search was invalid.
+			res.status(404)
+				.json({message:"No city by that ID found"})
+		}
+		
 	})
 
 app.listen(7777,()=>{
